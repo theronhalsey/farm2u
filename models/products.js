9f199ef2897eff032445f3aaa7667430eccb7c23
dByler1 <dannybyler@gmail.com>
2018-05-20 10:39:36 -0400

    do not merge this with master

--- models/products.js
+++ models/products.js
@@ -1,3 +1,13 @@
+'use strict';
+
+var fs        = require('fs');
+var path      = require('path');
+var Sequelize = require('sequelize');
+var basename  = path.basename(__filename);
+var env       = process.env.NODE_ENV || 'development';
+var config    = require(__dirname + '/../config/config.json')[env];
+var db        = {};
+
 module.exports = function (sequelize, DataTypes) {
     var Product = sequelize.define("Product", {
         productName: {
