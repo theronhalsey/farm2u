e4cd4801a0b7d9e6142c09d7649cff2c5c00572e
Theron Halsey <theron.halsey@gmail.com>
2018-05-22 17:44:06 -0400

    new database schema

--- models/products.js
+++ models/products.js
@@ -7,6 +7,13 @@ module.exports = function (sequelize, DataTypes) {
                 len: [1]
             }
         },
+        productTypeName: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [1]
+            }
+        },
         productAvailable: {
             type: DataTypes.BOOLEAN,
             allowNull: false,
@@ -17,21 +24,15 @@ module.exports = function (sequelize, DataTypes) {
             validate: {
                 len: [1]
             }
-        },
-        productID: {
-            type: DataTypes.INTEGER,
-            primaryKey: true,
-            autoIncrement: true
         }
     });
 
     Product.associate = function (models) {
-        Product.belongsTo(models.ProductType, {
+        /* Product.belongsTo(models.ProductType, {
             foreignKey: "typeID",
             allowNull: false
-            })
+            }) */
         Product.belongsTo(models.Farmer, {
-            foreignKey: "farmID",
             allowNull: false
             })
     };
